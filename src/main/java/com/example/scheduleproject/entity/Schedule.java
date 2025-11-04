9a2728d522361726a290f487b4cc6a87ed2f96d3
castlejuyeon <jucess1106@naver.com>
2025-11-04 11:43:27 +0900

    Feat: 공통 엔티티(BaseEntity) 추가로 createdAt, updatedAt 자동 관리
    - Schedule 엔티티에서 BaseEntity 상속받아 작성일/수정일 자동 처리

--- src/main/java/com/example/scheduleproject/entity/Schedule.java
+++ src/main/java/com/example/scheduleproject/entity/Schedule.java
@@ -10,28 +10,29 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name="schedules")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Schedule {
+public class Schedule extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long schedule_id;
     @Column(length = 100, nullable = false)
     private String title;
+    @Column(length = 200, nullable = false)
     private String content;
     @Column(length = 20, nullable = false)
     private String name;
     @Column(length = 20, nullable = false)
     private String password;
-    @Column(unique = true, nullable = false)
-    private String email;
 
-    public Schedule(String title, String content, String name, String password, String email) {
-        this.title=title;
-        this.content=content;
-        this.name=name;
+    public Schedule(String title, String content, String name, String password) {
+        this.title = title;
+        this.content = content;
+        this.name = name;
         this.password=password;
-        this.email=email;
     }
 
-
+    public void updateSchedule(String title, String name){
+        this.title=title;
+        this.name=name;
+    }
 }
