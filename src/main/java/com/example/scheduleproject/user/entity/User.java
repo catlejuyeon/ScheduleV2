b0c14ae52d8cdc07f7d9883ca7f9d250a751bbc9
castlejuyeon <jucess1106@naver.com>
2025-11-10 17:53:54 +0900

    👥Feat: User 엔티티 생성

---
+++ src/main/java/com/example/scheduleproject/user/entity/User.java
@@ -0,0 +1,33 @@
+package com.example.scheduleproject.user.entity;
+
+import com.example.scheduleproject.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name="users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userid;
+
+    @Column(nullable = false, length = 20)
+    private String username;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
